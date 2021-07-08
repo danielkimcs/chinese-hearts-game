da4e32cebb7947fc3aa064f40a611a0d205f51e4
dknj11902 <dankim20@bergen.org>
2021-07-08 19:04:03 -0400

    Added 404 page

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,17 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/home';
 import Room from './components/room';
+import NotFound from './components/not-found';
 
-const routes = [
-    { path: '/', name: 'Home', Component: Home },
-    { path: '/games/:roomName', name: 'Room', Component: Room },
-]
+// const routes = [
+//     { path: '/', name: 'Home', Component: Home },
+//     { path: '/games/:roomName', name: 'Room', Component: Room },
+// ]
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/games/:roomName" component={Room} />
+            <Route component={NotFound} />
         </Switch>
     );
 }
