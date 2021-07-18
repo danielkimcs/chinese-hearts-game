ed6f39c6699e1bdf4c38b0cbbcba743d42daa975
dknj11902 <dankim20@bergen.org>
2021-07-18 00:50:58 -0400

    Added critical import statement

--- client/src/components/room/components/player-list/components/player/components/playerContainers.js
+++ client/src/components/room/components/player-list/components/player/components/playerContainers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectedCardsComponent from './collectedCardsComponent';
 import PlayerUsernameTag from './playerUsernameTag';
 import FaceDownCardContainer from './faceDownCardContainer';
 
